1c5e5e61bcdc1db871c04fa23575058be7ae38e8
BuildTools <unconfigured@null.spigotmc.org>
2023-10-03 23:54:02 +0200

    try vosk as Hotword and whisper api as main

--- ls2.py
+++ ls2.py
@@ -2,9 +2,11 @@ import vosk
 import speech_recognition
 
 rec = speech_recognition.Recognizer()
-#top = vosk.Model("model")
+
+
 with speech_recognition.Microphone() as source:
     print("Say something!")
     audio = rec.listen(source)
 
-print(rec.recognize_vosk(audio,))
+print(rec.recognize_vosk(audio))
+# print(rec.recognize_whisper_api(audio))
