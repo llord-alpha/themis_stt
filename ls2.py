8177b972d9e562c28d0450bbbe9e02170bf95bbe
BuildTools <unconfigured@null.spigotmc.org>
2023-10-03 17:21:34 +0200

    speech recog

--- ls2.py
+++ ls2.py
@@ -1,4 +1,10 @@
 import vosk
 import speech_recognition
 
-rec = speech_recognition.recognizer_instance.recognize_vosk
+rec = speech_recognition.Recognizer()
+top = vosk.Model("model")
+with speech_recognition.Microphone() as source:
+    print("Say something!")
+    audio = rec.listen(source)
+
+print(rec.recognize_vosk(audio, model=top))
